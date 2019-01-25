73cd4128722045385a64dc218d45d45070bd99c9
Justin Prindle <justindprindle@gmail.com>
2019-01-24 22:45:12 -0500

    Initial solve

--- Problem1.hs
+++ Problem1.hs
@@ -3,7 +3,19 @@
 module Problem1 where
 
 -- Data type for an ordered binary trees
+-- TODO: should the trees contain only unique elements?
 data Bin a = EmptyTree | TreeNode a (Bin a) (Bin a)
 
+singleton :: a -> Bin a
+singleton x = TreeNode x EmptyTree EmptyTree
+
 insert :: Ord a => a -> Bin a -> Bin a
-insert = undefined
+insert x EmptyTree = singleton x
+insert x tree@(TreeNode n lower higher) =
+  case compare x n of
+    EQ -> tree
+    -- Assuming the trees aren't unique:
+    -- EQ -> TreeNode n (insert x lower) higher
+    LT -> TreeNode n (insert x lower) higher
+    GT -> TreeNode n lower (insert x higher)
+
