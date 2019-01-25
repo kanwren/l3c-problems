f16af3cdb0f7c3446c2dbe6ccf2ca8386819df35
Justin Prindle <justindprindle@gmail.com>
2019-01-24 22:50:25 -0500

    Add clarifying comments

--- Problem3.hs
+++ Problem3.hs
@@ -1,12 +1,14 @@
 -- Largest Contiguous Sum
 
--- The simplest way to solve the largest contiguous sum is using Kadane's
--- algorithm, which solves it in O(n) time and O(1) space
+-- The simplest way to solve the largest contiguous sum problem is using
+-- Kadane's algorithm, which solves it in O(n) time and O(1) space
 
 module Problem3 where
 
 import Data.List (foldl')
 
+-- Need a strict fold (foldl') to avoid stack overflows from lazy foldl
+-- This is more efficient over Integers than lazy evaluation
 maxContSum :: [Integer] -> Integer
 maxContSum = snd . foldl' kadane (0, 0)
   where kadane (curMax, totalMax) x =
