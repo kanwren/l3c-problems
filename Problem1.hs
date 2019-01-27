485b836ba90f62f5eea13d3c3d65987c23772ad1
Justin Prindle <justindprindle@gmail.com>
2019-01-27 18:06:22 -0500

    Update and tidy up according to clarifications

--- Problem1.hs
+++ Problem1.hs
@@ -2,20 +2,19 @@
 
 module Problem1 where
 
--- Data type for an ordered binary trees
+-- Data type for a unique ordered binary trees
 data Bin a = EmptyTree | TreeNode a (Bin a) (Bin a)
 
-singleton :: a -> Bin a
-singleton x = TreeNode x EmptyTree EmptyTree
+leaf :: a -> Bin a
+leaf x = TreeNode x EmptyTree EmptyTree
 
+-- Recursively insert element into ordered binary tree
 insert :: Ord a => a -> Bin a -> Bin a
-insert x EmptyTree = singleton x
+insert x EmptyTree = leaf x
 insert x (TreeNode n lower higher) =
   case compare x n of
-    -- Update data in case of non-standard Ord/Eq implementation for a
+    -- Update data in case of non-standard Ord/Eq implementation over a
     EQ -> TreeNode x lower higher
-    -- Assuming the trees aren't unique:
-    -- EQ -> TreeNode n (insert x lower) higher
     LT -> TreeNode n (insert x lower) higher
     GT -> TreeNode n lower (insert x higher)
 
